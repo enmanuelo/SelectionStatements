d9e9739626095b9a325db2ff2491839e8eb6a653
Enmanuel Ozoria <ozoriaeman1998@gmail.com>
2022-07-07 12:36:40 -0400

    SelectionStatements exercise

---
+++ SelectionStatements/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace SelectionStatements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var rand = new Random();
+            int favNum = rand.Next(1, 1000);
+            Console.WriteLine("Guess my favorite number: ");
+            var userNum = int.Parse(Console.ReadLine());
+
+
+            if (userNum < favNum)
+            {
+                Console.WriteLine("too low");
+            }
+
+            else if (userNum > favNum)
+            {
+                Console.WriteLine("too high");
+            }
+
+            else
+            {
+                Console.WriteLine("You read my mind!");
+            }
+
+        }
+    }
+
+}
