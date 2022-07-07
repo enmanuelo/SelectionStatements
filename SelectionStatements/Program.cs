b9b793924ffb7a33167d8b0048128aca4d856fc4
Enmanuel Ozoria <ozoriaeman1998@gmail.com>
2022-07-07 14:42:07 -0400

    corrected format

--- SelectionStatements/Program.cs
+++ SelectionStatements/Program.cs
@@ -1,33 +1,22 @@
-﻿using System;
+﻿// Exercise 1
 
-namespace SelectionStatements
+var rand = new Random();
+int favNum = rand.Next(1, 100);
+Console.WriteLine("Guess my favorite number");
+var userNum = int.Parse(Console.ReadLine());
+
+
+if (userNum < favNum)
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            var rand = new Random();
-            int favNum = rand.Next(1, 1000);
-            Console.WriteLine("Guess my favorite number: ");
-            var userNum = int.Parse(Console.ReadLine());
+    Console.WriteLine("too low");
+}
 
+else if (userNum > favNum)
+{
+    Console.WriteLine("too high");
+}
 
-            if (userNum < favNum)
-            {
-                Console.WriteLine("too low");
-            }
-
-            else if (userNum > favNum)
-            {
-                Console.WriteLine("too high");
-            }
-
-            else
-            {
-                Console.WriteLine("You read my mind!");
-            }
-
-        }
-    }
-
+else
+{
+    Console.WriteLine("You read my mind!");
 }
